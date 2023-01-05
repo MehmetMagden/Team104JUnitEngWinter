d90ea907093493b71f0e3e690671f79f29221b5d
Mehmet Maden <yasamtr@gmail.com>
2023-01-05 17:21:21 +0300

    Frist

---
+++ src/test/java/C01_ByeByeMainMethod.java
@@ -0,0 +1,38 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class C01_ByeByeMainMethod {
+
+
+
+    @After
+    public void afterMethod(){
+        System.out.println("After method is executed");
+    }
+
+    @Before
+    public void beforemethod(){
+        System.out.println("before method is executed");
+    }
+
+    @Test
+    public void test01(){
+        System.out.println("Test 01 is executed");
+    }
+
+    @Test
+    public void test02(){
+        System.out.println("Test 02 is executed");
+    }
+
+    @Test
+    public void test03(){
+        System.out.println("Test 03 is executed");
+    }
+
+
+
+
+}
---
+++ src/test/java/C02_Before_After.java
@@ -0,0 +1,55 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C02_Before_After {
+
+    // create 3 different tests
+    // before every  method, create a driver
+    // 1 - amazon
+    // 2- wisequarter
+    // 3- youtube
+    // visit these websites and print the titles and close drivers
+   WebDriver driver;
+
+    @Before
+   public void setUp(){
+       WebDriverManager.chromedriver().setup();
+       driver = new ChromeDriver();
+       driver.manage().window().maximize();
+       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+   }
+
+   @After
+   public void tearDown(){
+       driver.close();
+   }
+
+    @Test
+    public void test01(){
+        driver.get("https://www.amazon.com");
+        System.out.println(driver.getTitle());
+    }
+
+    @Test
+    public void test02(){
+        driver.get("https://www.wisequarter.com");
+        System.out.println(driver.getTitle());
+    }
+
+    @Test
+    public void test03(){
+        driver.get("https://www.youtube.com");
+        System.out.println(driver.getTitle());
+    }
+
+
+
+
+}
